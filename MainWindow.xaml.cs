16190403a2f62afbb6e6469b9241539b26901843
icychick <icychick@outlook.com>
2026-06-14 11:08:54 +0800

    fix(build): replace COMReference with standard Reference and generate interops via aximp in workflow, fix namespace clashes by disabling ImplicitUsings

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 using rdpManager.Helpers;
 using rdpManager.Views;
